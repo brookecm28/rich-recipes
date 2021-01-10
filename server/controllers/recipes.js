34084b74796678f25231ebb46c1af4400fabac38
Brooke Miller <brookecm28@gmail.com>
2021-01-09 21:36:14 -0700

    Full DB additions complete

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -9,16 +9,12 @@ module.exports = {
         if (req.session.user) {
             const {id} = req.session.user
             let recipeAdd
-            let ingredientAdd
-            let instructionAdd
             try {
             //send the recipe to the db
             recipeAdd = await db.recipes.add_recipe([id, title, photo])
             //grab the newly assigned recipe_id
             let rrId = recipeAdd[0].recipe_id 
             //send the ingredients to the db with the correct recipe_id
-            // ingredientAdd = await db.recipes.add_ingredients([rrId, ingredients])
-            // console.log(ingredientAdd)
             ingredients.forEach(async (ingObj) => {
                 await db.recipes.add_ingredients({
                     recipe_id: rrId,
@@ -28,22 +24,22 @@ module.exports = {
                 })
             })
             //send the instructions to the db  with the correct recipe_id
-            instructionAdd = await db.recipes.add_instructions([rrId, instructions])
+            instructions.forEach(async (instrObj) => {
+                await db.recipes.add_instructions({
+                    recipe_id: rrId,
+                    step_number: instrObj.step,
+                    instruction: instrObj.instruction
+                })
+            })
+            // instructionAdd = await db.recipes.add_instructions([rrId, instructions])
             } catch(err) {
                 console.log(err)
             }
             //combine results so they can all be sent back in the response
-            let complete = {
-                recipe: recipeAdd[0].data,
-                ingredients: ingredientAdd[0].data,
-                instructions: instructionAdd[0].data
-            }
-            res.status(200).send(complete)
-            // .catch(err => console.log(err))
+            res.status(200).send(recipeAdd[0])
+        //if not logged in    
         } else {
             return res.status(403).send('User not logged in.')
         }
     }
 } 
-
-// destructure the response, create object after 3rd request with 3 props: recipe, ingr, instr; value will be recipeAdd.data, ingredientAdd.data, instructionAdd.data; then send this obj back
