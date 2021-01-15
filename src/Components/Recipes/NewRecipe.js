d3a7fe6ec0910247f96e0ae324d82567f6c33e2a
Brooke Miller <brookecm28@gmail.com>
2021-01-15 16:40:43 -0700

    NewRecipe updates & display of ingredients as they're added

--- src/Components/Recipes/NewRecipe.js
+++ src/Components/Recipes/NewRecipe.js
@@ -25,42 +25,36 @@ class NewRecipe extends Component {
         this.setState ({
             title: e
         })
-        console.log(this.state.title)
     }
 
     handleImageChange(e) {
         this.setState ({
             img: e
         })
-        console.log(this.state.img)
     }
     
     handleQuantityChange(e) {
         this.setState ({
             quantity: e
         })
-        console.log(this.state.quantity)
     }
 
     handleMeasurementchange(e) {
         this.setState ({
             measurement: e
         })
-        console.log(this.state.measurement)
     }
 
     handleIngredientChange(e) {
         this.setState ({
             ingredient: e
         })
-        console.log(this.state.ingredient)
     }
 
     handleInstructionChange (e) {
         this.setState ({
             tempInstructions: e
         })
-        console.log(this.state.tempInstructions)
     }
 
     addIngredient() {
@@ -76,7 +70,6 @@ class NewRecipe extends Component {
             measurement: '',
             ingredient: ''
         })
-        console.log(this.state.ingredients)
     }
 
     addRecipe() {
@@ -88,13 +81,10 @@ class NewRecipe extends Component {
                 step: i + 1,
                 instruction: stringInstructions[i]
             })
-            console.log(formattedInstructions)
         }
         this.setState({
             instructions: [formattedInstructions]
         })
-        console.log(this.state.instructions)
-        console.log(this.state.completeRecipe)
         axios.post('/api/recipes/new', {
             title: this.state.title,
             ingredients: this.state.ingredients,
@@ -109,45 +99,58 @@ class NewRecipe extends Component {
     }
     
     render () {
+        let {ingredients} = this.state
+        let counter = 0;
+        let mappedIngredients = ingredients.map(ingredient => {
+            return (
+                <div className='single-ingredient-container' key={counter + 1}>
+                    <p className='ing-list'>{ingredient.quantity} </p>
+                    <p className='ing-list'>{ingredient.measurement} </p>
+                    <p className='ing-list'>{ingredient.ingredient}</p>
+                </div>
+            )
+        })
         return (
         <div>
             <UserHeader />
             <div className='body'>
                 <div className='content-container'>
-                    <div>
-                        <div className='group-title'>Title</div>
-                        <input 
-                            className='inputs'
-                            placeholder='Title' 
-                            value={this.state.title}
-                            onChange={e => this.handleTitleChange(e.target.value)}>
-                        </input>
-                    </div>
-                    <div>
-                        <div className='group-title'>Image</div>
-                        <input //this will be amazon s3
-                            className='inputs'
-                            placeholder='Image'
-                            value={this.state.img}
-                            onChange={e => this.handleImageChange(e.target.value)}>
-                        </input>
+                    <div className='top-group'>
+                        <div>
+                            <div className='group-title'>Title</div>
+                            <input
+                                className='inputs'
+                                placeholder='Title'
+                                value={this.state.title}
+                                onChange={e => this.handleTitleChange(e.target.value)}>
+                            </input>
+                        </div>
+                        <div>
+                            <div className='group-title'>Image</div>
+                            <input //this will be amazon s3
+                                className='inputs'
+                                placeholder='Image'
+                                value={this.state.img}
+                                onChange={e => this.handleImageChange(e.target.value)}>
+                            </input>
+                        </div>
                     </div>
                     <div className='ingredient-container'>
                         <div className='group-title'>Ingredients</div>
                         <input 
-                            className='inputs'
+                            className='inputs ing-input'
                             placeholder='Quantity' 
                             value={this.state.quantity} 
                             onChange={e => this.handleQuantityChange(e.target.value)}>
                         </input>
                         <input 
-                            className='inputs'
+                            className='inputs ing-input'
                             placeholder='Measurement' 
                             value={this.state.measurement}
                             onChange={e => this.handleMeasurementchange(e.target.value)}>
                         </input>
                         <input 
-                            className='inputs'
+                            className='inputs ing-input'
                             placeholder='Ingredient' 
                             value={this.state.ingredient}
                             onChange={e => this.handleIngredientChange(e.target.value)}>
@@ -159,6 +162,8 @@ class NewRecipe extends Component {
                         <div className='text-instr'>Press Enter for a new line; a new line = a new ingredient</div>
                         <textarea 
                             name='text-area'
+                            rows='14'
+                            cols='75'
                             className='text-area'
                             placeholder='Instructions' 
                             value={this.state.tempInstructions}
@@ -167,6 +172,11 @@ class NewRecipe extends Component {
                     </div>
                     <button className='btn add-recipe'
                         onClick={ this.addRecipe}>Add Recipe</button>
+                    <div className='ing-list-container-container'>
+                        <div className='ing-list-container'>
+                            {mappedIngredients}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
